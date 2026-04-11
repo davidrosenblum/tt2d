aa2db9689adb47105bb83bf804fef7dfa1c15614
David Rosenblum <david@rosenblum.net>
2026-04-11 18:26:36 -0400

    app constants

---
+++ app/src/constants.rs
@@ -0,0 +1,13 @@
+// Screen viewport size
+pub const VIEWPORT_WIDTH: u32 = 1280;
+pub const VIEWPORT_HEIGHT: u32 = 720;
+
+// Size of sprites when rendered
+pub const TILE_SIZE: f32 = 64.;   // Source is 32x32
+pub const COG_SIZE: f32 = 96.;    // Source is 48x48
+pub const TOON_SIZE: f32 = 96.;   // Source is 48x48
+
+// Z-Index values for map layer position
+pub const TERRAIN_Z_INDEX: f32 = 10.;
+pub const STRUCTURE_BASE_Z_INDEX: f32 = 20.;
+pub const STRUCTURE_TOP_Z_INDEX: f32 = 30.;
